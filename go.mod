59bf047bc6bebf8b82594cb8366c0ed3a6229f3e
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-10 19:20:42 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/EduardoZepeda/goRestWebSocketExample
+
+go 1.15
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.5
+	github.com/segmentio/ksuid v1.0.4
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/EduardoZepeda/goRestWebSocketExample/handlers"
+	"github.com/EduardoZepeda/goRestWebSocketExample/server"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_CONNECTION := os.Getenv("DATABASE_CONNECTION")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:          JWT_SECRET,
+		Port:               PORT,
+		DatabaseConnection: DATABASE_CONNECTION,
+	})
+
+	if err != nil {
+		log.Fatal("ListenAndServe", err)
+	}
+
+	s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+	r.HandleFunc("/signup", handlers.SignUpHandler(s)).Methods(http.MethodPost)
+
+}
