89456aca193856450314752ef0eae7cd111e1d26
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-05-18 14:29:30 -0500

    Add CRUD operations for posts

--- handlers/post.go
+++ handlers/post.go
@@ -9,16 +9,21 @@ import (
 	"github.com/EduardoZepeda/goRestWebSocketExample/repository"
 	"github.com/EduardoZepeda/goRestWebSocketExample/server"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/gorilla/mux"
 	"github.com/segmentio/ksuid"
 )
 
-type InsertPostRequest struct {
+type UpsertPostRequest struct {
 	PostContent string `json:"post_content"`
 }
 
 type PostResponse struct {
 	Id          string `json:"id"`
 	PostContent string `json:"post_content"`
+}
+
+type PostUpdateResponse struct {
+	Message string `json:"message"`
 }
 
 func InsertPostHandler(s server.Server) http.HandlerFunc {
@@ -32,7 +37,7 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 			return
 		}
 		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
-			var postRequest = InsertPostRequest{}
+			var postRequest = UpsertPostRequest{}
 			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
 				return
 			}
@@ -54,6 +59,87 @@ func InsertPostHandler(s server.Server) http.HandlerFunc {
 			}
 			w.Header().Set("Content-Type", "application/json")
 			json.NewEncoder(w).Encode(PostResponse{Id: post.Id, PostContent: post.PostContent})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func GetPostByIdHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		post, err := repository.GetPostById(r.Context(), params["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if post.Id == "" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(404)
+			json.NewEncoder(w).Encode(PostUpdateResponse{Message: "Not found"})
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(post)
+		}
+	}
+}
+
+func UpdatePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(s.Config().JWTSecret), nil
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			var postRequest = UpsertPostRequest{}
+			if err := json.NewDecoder(r.Body).Decode(&postRequest); err != nil {
+				return
+			}
+			post := models.Post{
+				Id:          params["id"],
+				PostContent: postRequest.PostContent,
+				// Remember to use claims.UserId, not claims.Id
+				UserId: claims.UserId,
+			}
+			err = repository.UpdatePost(r.Context(), &post)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostUpdateResponse{Message: "Post updated"})
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func DeletePostHandler(s server.Server) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		tokenString := strings.TrimSpace(r.Header.Get("Authorization"))
+		token, err := jwt.ParseWithClaims(tokenString, &models.AppClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(s.Config().JWTSecret), nil
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if claims, ok := token.Claims.(*models.AppClaims); ok && token.Valid {
+			err = repository.DeletePost(r.Context(), params["id"], claims.UserId)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(PostUpdateResponse{Message: "Post deleted"})
 		} else {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
